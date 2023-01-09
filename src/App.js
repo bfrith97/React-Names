2525114eff58f76f4172a90fdc0ce1b04e6733a6
Brandon <brandonfrith@hotmail.co.uk>
2023-01-09 14:58:30 +0000

    add adding functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import { useState } from "react";
 
-functuon App() {
-    return (
-        <div>
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
-        </div>
-    );
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}]
+    });
+  }
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/UI/Button.js
@@ -0,0 +1,7 @@
+import classes from './Button.module.css'
+
+const Button = props => {
+    return <button className={classes.button} type='submit'>Add User</button>
+}
+
+export default Button;
